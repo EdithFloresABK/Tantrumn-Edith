786d543b6fa3577125fe57a5cc765325f0188927
fhosn <firashosn@gmail.com>
2021-05-11 01:40:10 +0100

    week 11 - AI Integration
    added sprint and pull object support for AI
    AI finish race support
    EQS to find throwable object and EQS test pawn

--- Source/Tantrumn/TantrumnGameStateBase.cpp
+++ Source/Tantrumn/TantrumnGameStateBase.cpp
@@ -7,36 +7,53 @@
 #include "TantrumnPlayerController.h"
 #include "TantrumnPlayerState.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "TantrumnAIController.h"
+
+void ATantrumnGameStateBase::UpdateResults(ATantrumnPlayerState* PlayerState, ATantrumnCharacterBase* TantrumnCharacter)
+{
+	if (!PlayerState || !TantrumnCharacter)
+	{
+		return;
+	}
+
+	const bool IsWinner = Results.Num() == 0;
+	PlayerState->SetIsWinner(IsWinner);
+	//ensureAlwaysMsgf(IsWinner, TEXT("ATantrumnCharacterBase::OnMontageEnded Winner Logic Broken"));
+	PlayerState->SetCurrentState(EPlayerGameState::Finished);
+
+	FGameResult Result;
+	Result.Name = TantrumnCharacter->GetName();
+	//TODO get the actual time it took in order to post to a leaderboard/results widget
+	Result.Time = 5.0f;
+	Results.Add(Result);
+}
+
 //only ever called by the authority
 void ATantrumnGameStateBase::OnPlayerReachedEnd(ATantrumnCharacterBase* TantrumnCharacter)
 {
 	ensureMsgf(HasAuthority(), TEXT("ATantrumnGameStateBase::OnPlayerReachedEnd being called from Non Authority!"));
+
+	//two cases, Player or AI reaches the end
 	if (ATantrumnPlayerController* TantrumnPlayerController = TantrumnCharacter->GetController<ATantrumnPlayerController>())
 	{
-		
 		TantrumnPlayerController->ClientReachedEnd();
 		TantrumnCharacter->GetCharacterMovement()->DisableMovement();
-
 		ATantrumnPlayerState* PlayerState = TantrumnPlayerController->GetPlayerState<ATantrumnPlayerState>();
-		if (PlayerState)
-		{
-			const bool IsWinner = Results.Num() == 0;
-			PlayerState->SetIsWinner(IsWinner);
-			PlayerState->SetCurrentState(EPlayerGameState::Finished);
-		}
-
-		FGameResult Result;
-		Result.Name = TantrumnCharacter->GetName();
-		//TODO get the actual time it took in order to post to a leaderboard/results widget
-		Result.Time = 5.0f;
-		Results.Add(Result);
+		UpdateResults(PlayerState, TantrumnCharacter);
 
 		//TODO this will not work once JIP(Join In Progress) is enabled
-		if (Results.Num() == PlayerArray.Num())
+		if (Results.Num() >= PlayerArray.Num())
 		{
 			GameState = EGameState::GameOver;
 		}
 	}
+	else if (ATantrumnAIController* TantrumnAIController = TantrumnCharacter->GetController<ATantrumnAIController>())
+	{
+		ATantrumnPlayerState* PlayerState = TantrumnAIController->GetPlayerState<ATantrumnPlayerState>();
+		UpdateResults(PlayerState, TantrumnCharacter);
+		TantrumnAIController->OnReachedEnd();
+	}
+
 }
 
 void ATantrumnGameStateBase::ClearResults()
@@ -49,7 +66,7 @@ void ATantrumnGameStateBase::GetLifetimeReplicatedProps(TArray< FLifetimePropert
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 	FDoRepLifetimeParams SharedParams;
-	SharedParams.bIsPushBased = true;
+	//SharedParams.bIsPushBased = true;
 	//SharedParams.Condition = COND_SkipOwner;
 
 	DOREPLIFETIME_WITH_PARAMS_FAST(ATantrumnGameStateBase, GameState, SharedParams);
